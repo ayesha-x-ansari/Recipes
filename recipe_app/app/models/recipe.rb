0d5a1a668c99e1385b9005d5a8ace3c9b6a36484
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-07-23 03:04:53 +0000

    added comment for recipe

--- recipe_app/app/models/recipe.rb
+++ recipe_app/app/models/recipe.rb
@@ -3,4 +3,6 @@ class Recipe < ApplicationRecord
   validates :description, presence: true
 
   belongs_to :user
+  has_many :comments
+
 end
--- recipe_app/config/routes.rb
+++ recipe_app/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :recipes
-  resources :comments
+  resources :recipes  do
+    resources :comments
+  end
   root 'recipes#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
 end
+  
