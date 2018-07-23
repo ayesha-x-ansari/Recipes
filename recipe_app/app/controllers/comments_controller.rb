96970fe8280ae5e2d6732117849a9736b274b1ce
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-07-23 01:15:37 +0000

    added comment model

---
+++ recipe_app/app/controllers/comments_controller.rb
@@ -0,0 +1,56 @@
+class CommentsController < ApplicationController
+
+  before_action :set_recipe
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.valid?
+      @recipe.comments << @comment
+      @recipe.save
+      @comment.save
+      redirect_to @recipe, alert: "Comment successfully added."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+    if @comment.user_id != current_user.id
+      redirect_to user_recipes_path(current_user), alert: "You may not delete this comment!"
+    end
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    @comment.save
+    redirect_to @recipe
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    @recipe.save
+    redirect_to @recipe, alert: "Comment successfully deleted."
+
+    if @comment.user_id != current_user.id
+      redirect_to user_recipes_path(current_user), alert: "You may not delete this comment."
+    end
+  end
+
+  private
+
+    def set_recipe
+      @recipe = Recipe.find_by(id: params[:recipe_id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:content, :user_id, :recipe_id)
+    end
+end      
--- recipe_app/db/schema.rb
+++ recipe_app/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_21_231838) do
+ActiveRecord::Schema.define(version: 2018_07_23_004029) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
