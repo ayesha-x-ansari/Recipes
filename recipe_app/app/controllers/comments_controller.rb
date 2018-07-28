e92ab08b3700363d5f0172fc4c34e5c8c0b5d061
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-07-28 23:57:02 +0000

    added check boc for comment ratings

--- recipe_app/app/controllers/comments_controller.rb
+++ recipe_app/app/controllers/comments_controller.rb
@@ -59,6 +59,6 @@ class CommentsController < ApplicationController
     end
 
     def comment_params
-      params.require(:comment).permit(:content, :user_id, :recipe_id)
+      params.require(:comment).permit(:content, :user_id, :recipe_id, :ratings)
     end
 end
