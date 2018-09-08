2b5c7d9d2eaabf365b2e0e1563703b9ce048a4b2
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-09-08 04:50:35 +0000

    working on show recipe

--- recipe_app/app/assets/javascripts/comments.js
+++ recipe_app/app/assets/javascripts/comments.js
@@ -1,17 +1,14 @@
-/ The below code is for comments on formulas show page, /formulas/#
-// "Create Comment" button creates a new comment object, saves and appends back into the DOM
-
-$(function(){
+$(function () {
   // on submit of form class="new_comment"
-  $("form").on("submit", function(event) {
-    debugger
-
+  $('form').on("submit", function(event) {
     $.ajax({
       type: "POST",
       url: this.action, // this refers to whatever triggered the action === [object HTMLFormElement]
       data: $(this).serialize(), // takes our form data and serializes it
       success: function(response) {
+        event.preventDefault();
         // on success, update the DOM with response in the form of data
+        //$form.find("input[type=submit]").removeAttr('disabled');
         let comment = new Comment(response);
         comment.renderComments();
         $(".commentBox").val("");
@@ -19,28 +16,30 @@ $(function(){
     });
     event.preventDefault();
   })
-});
-
+}).error(function(notNeeded){
+alert("we broke!!!!")
+ });
 // a new comment's response is passed as data and attributes are set to 'this'.
 function Comment(data) {
   this.id = data.id;
   this.content = data.content;
+  this.recipe_id  = data.recipe_id;
   this.user = data.user;
 };
-
 // this method appends html to the div id="submitted-comments"
 Comment.prototype.renderComments = function() {
   let html = "" ;
   html +=
-  `<br>
-    <div class=\'card' id=\'comment-${this.id}'>
-    <div class=\'card-body'>
-      <h6 class=\'card-subtitle mb-2 text-muted'>Posted by:
-        <a href='/users/${this.user.id}'>${this.user.username}</a>
-      </h6>
-      <p class=\'card-text'>${this.content}</p>
-    </div>
-  </div>`;
-
+  //<h1 class=\'card-subtitle mb-2 text-muted'>  <a href='/recipes/${this.recipe_id}/comments/${this.id/edit}'>  Edit comment </a> </h1>
+  //<h6 class=\'card-subtitle mb-2 text-muted'>
+//  <h1><a href='/users/${this.user.id}'>${this.user.name} </a> </h1>
+  ` <div class=\'col=md-8 col-md-offset-1'>
+    <p>Comment By: ${this.user.name} - ${this.content}
+    <br>
+    <a href='/recipes/${this.recipe_id}/comments/${this.id}/edit'>  Edit Comment</a>   <a data-confirm="Are you sure?" class="btn btn-link" rel="nofollow" data-method="delete" href="/recipes/${this.recipe_id}/comments/${this.id}">Delete Comment</a>
+    </p>
+    </div>`;
   $("#submitted-comments").append(html);
+  $(".commentBox").val("");
+  $(".ratingSelection").val("");
 };
--- recipe_app/app/assets/javascripts/recipes.js
+++ recipe_app/app/assets/javascripts/recipes.js
@@ -1,16 +1,13 @@
-$(function () {
 
+$(function () {
   // on submit of form class="new_comment"
   $('form').on("submit", function(event) {
-
     $.ajax({
       type: "POST",
       url: this.action, // this refers to whatever triggered the action === [object HTMLFormElement]
       data: $(this).serialize(), // takes our form data and serializes it
       success: function(response) {
         event.preventDefault();
-
-
         // on success, update the DOM with response in the form of data
         //$form.find("input[type=submit]").removeAttr('disabled');
         let comment = new Comment(response);
@@ -23,7 +20,6 @@ $(function () {
 }).error(function(notNeeded){
 alert("we broke!!!!")
  });
-
 // a new comment's response is passed as data and attributes are set to 'this'.
 function Comment(data) {
   this.id = data.id;
@@ -31,25 +27,20 @@ function Comment(data) {
   this.recipe_id  = data.recipe_id;
   this.user = data.user;
 };
-
 // this method appends html to the div id="submitted-comments"
 Comment.prototype.renderComments = function() {
   let html = "" ;
   html +=
-
   //<h1 class=\'card-subtitle mb-2 text-muted'>  <a href='/recipes/${this.recipe_id}/comments/${this.id/edit}'>  Edit comment </a> </h1>
   //<h6 class=\'card-subtitle mb-2 text-muted'>
 //  <h1><a href='/users/${this.user.id}'>${this.user.name} </a> </h1>
   ` <div class=\'col=md-8 col-md-offset-1'>
-
     <p>Comment By: ${this.user.name} - ${this.content}
     <br>
     <a href='/recipes/${this.recipe_id}/comments/${this.id}/edit'>  Edit Comment</a>   <a data-confirm="Are you sure?" class="btn btn-link" rel="nofollow" data-method="delete" href="/recipes/${this.recipe_id}/comments/${this.id}">Delete Comment</a>
     </p>
     </div>`;
-
   $("#submitted-comments").append(html);
   $(".commentBox").val("");
   $(".ratingSelection").val("");
-
 };
